6516528bbcd86dfd6d25640a1c5d0023dabd52c7
Cannon <cannonrivera45@gmail.com>
2025-11-05 15:10:32 -0600

    fixed

--- Java/RandomDataFile/src/main/java/org/example/Main.java
+++ Java/RandomDataFile/src/main/java/org/example/Main.java
@@ -1,8 +1,8 @@
+//Cannon Rivera, 11/5/2025, RandomDataFile
+//The purpose of this program is to write 10 random numbers to a file
+//And then to display all the numbers
+
 package org.example;
-//Program 2: Write a program to create a new file titled data.file, if the file does not exist.
-// Then write to the new file, adding 10 randomly generated numbers,
-// or append 10 randomly generated numbers to a previous file. Each integer is to be separated by a space.
-// Close the file, then reopen the file and read the data from the file and display it.
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -11,7 +11,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        File file = new File("data.file");
+        File file = new File("file.txt");
         Random random = new Random();
 
         try {
@@ -28,7 +28,7 @@ public class Main {
             return;
         }
 
-        System.out.println("Contents of the data.file is:");
+        System.out.println("Contents of the file.txt is:");
         try (Scanner fileScanner = new Scanner(file)) {
             while (fileScanner.hasNext()) {
                 System.out.print(fileScanner.next() + " ");
